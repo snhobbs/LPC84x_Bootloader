8f7eaf8a59f6e9caf9a0a3c719639044e95d357a
Simon <simon.hobbs@electrooptical.net>
2020-10-14 12:37:07 -0400

    fixed crc issues

--- image_maker.py
+++ image_maker.py
@@ -4,9 +4,18 @@ f = open(sys.argv[1], 'rb')
 data = f.read()
 f.close()
 
-kApplicationSize = 28 * 1024
-if len(data) == 64 * 1024:
-    image = data[36*1024]
+kTotalSections = 64
+kSectorSize = 1024
+
+kBootloaderSections = 14
+
+kTotalSize = kTotalSections * kSectorSize
+kBootloaderSize = kBootloaderSections * kSectorSize
+kApplicationSectors = kTotalSections - kBootloaderSections
+kApplicationSize = (kApplicationSectors) * kSectorSize
+
+if len(data) == kTotalSize:
+    image = data[kBootloaderSize:]
 elif len(data) == kApplicationSize:
     image = data
 elif len(data) < kApplicationSize:
@@ -14,8 +23,14 @@ elif len(data) < kApplicationSize:
 else:
     raise UserWarning("Binary length incorrect")
 
+assert(len(image) == kApplicationSize)
+image = image[:-4] # remove last 4 bytes
 crc = zlib.crc32(image, 0)
-image_out = image[:-4] + crc.to_bytes(4, "little")
+image_out = image + crc.to_bytes(4, "little")
+assert(len(image_out) == kApplicationSize)
+assert(zlib.crc32(image_out[:-4], 0) == crc)
+print("Image CRC ", crc)
+
 with open("imageout.bin", "wb") as fout:
     fout.write(image_out)
 
